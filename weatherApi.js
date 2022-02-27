e3e4ed2b4f15e02fb5350f9f956384b0d008efdb
Mustafa Kendigüzel <72809479+mustafakendiguzel@users.noreply.github.com>
2022-02-27 18:40:45 +0300

    Update weatherApi.js

--- weatherApi.js
+++ weatherApi.js
@@ -4,6 +4,6 @@ export var options = {
   params: {location: `Istanbul`, format: 'json', u: 'c'},
   headers: {
     'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com',
-    'x-rapidapi-key': 'cae51047bcmsh8bc723d5a0fd645p110662jsn058c0fb08479'
+    'x-rapidapi-key': 'hidden'
   }
 };
